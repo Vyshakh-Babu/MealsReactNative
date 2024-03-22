8b4ad5b0e1ed3e5cf48b553cc93693220047a0ef
Vyshakh Babu <vyshakh.mmb@gmail.com>
2024-03-22 14:01:22 +0530

    Set basic styling for app screen as a pressable grid cards

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import { CategoryGridTitle } from "../components/CategoryGridTitle";
+
+function renderCategoryItem(itemData) {
+	return <CategoryGridTitle title={itemData.item.title} color={itemData.item.color} />;
+}
+
+const CategoriesScreen = () => {
+	return <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} renderItem={renderCategoryItem} numColumns={2} />;
+};
+
+export default CategoriesScreen;

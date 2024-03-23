424ea436a9ab5e685f9f6bee3d48004d811c0776
Vyshakh Babu <vyshakh.mmb@gmail.com>
2024-03-23 14:00:20 +0530

    Added basic styles to the meal item

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -6,7 +6,9 @@ import { CategoryGridTitle } from "../components/CategoryGridTitle";
 const CategoriesScreen = ({ navigation }) => {
 	function renderCategoryItem(itemData) {
 		function pressHandler() {
-			navigation.navigate("MealsOverview");
+			navigation.navigate("MealsOverview", {
+				categoryId: itemData.item.id,
+			});
 		}
 
 		return <CategoryGridTitle title={itemData.item.title} color={itemData.item.color} onPress={pressHandler} />;

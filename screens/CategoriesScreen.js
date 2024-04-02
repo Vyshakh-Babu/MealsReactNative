bd6ac966b112a025575e7fd4c7327f7773c61eb1
Vyshakh Babu <vyshakh.mmb@gmail.com>
2024-04-02 15:49:17 +0530

    Implemented simple notification system

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { FlatList } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 import { CategoryGridTitle } from "../components/CategoryGridTitle";
+import * as Notifications from "expo-notifications";
+import IconButton from "../components/IconButton";
+
+Notifications.setNotificationHandler({
+	handleNotification: async () => {
+		return {
+			shouldPlaySound: false,
+			shouldSetBadge: false,
+			shouldShowAlert: true,
+		};
+	},
+});
 
 const CategoriesScreen = ({ navigation }) => {
+	function scheduleNotificationHandler() {
+		Notifications.scheduleNotificationAsync({
+			content: {
+				title: "My first local notification",
+				body: "This is the notification body",
+				data: { userName: "Vyshakh" },
+			},
+			trigger: {
+				seconds: 5,
+			},
+		});
+	}
+
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			headerRight: () => {
+				return <IconButton icon="notifications-circle" color="white" size={72} onPress={scheduleNotificationHandler} />;
+			},
+		});
+	}, [navigation, scheduleNotificationHandler]);
+
 	function renderCategoryItem(itemData) {
 		function pressHandler() {
 			navigation.navigate("MealsOverview", {

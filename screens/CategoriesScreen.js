341ac93f439f19b7c01b3d517d860d275e67ab10
Vyshakh Babu <vyshakh.mmb@gmail.com>
2024-04-03 12:43:06 +0530

    Push notification from device with request permission

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useLayoutEffect } from "react";
-import { FlatList } from "react-native";
+import { Alert, FlatList, Platform } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 import { CategoryGridTitle } from "../components/CategoryGridTitle";
 import * as Notifications from "expo-notifications";
@@ -29,13 +29,34 @@ const CategoriesScreen = ({ navigation }) => {
 		});
 	}
 
+	//https://exp.host/--/api/v2/push/send is the endpoint provided by Expo for sending push notifications.
+	//to: The Expo push token of the device to which the notification should be sent. This token uniquely identifies the device and allows Expo to route the notification accordingly.
+	function sendPushNotificationHandler() {
+		fetch("https://exp.host/--/api/v2/push/send", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				to: "ExponentPushToken[LUqv0ePhp7uMN4XNlygv44]",
+				title: "Push Notification Test from device",
+				body: "This is a sample push notification",
+			}),
+		});
+	}
+
 	useLayoutEffect(() => {
 		navigation.setOptions({
 			headerRight: () => {
-				return <IconButton icon="notifications-circle" color="white" size={72} onPress={scheduleNotificationHandler} />;
+				return (
+					<>
+						<IconButton icon="notifications-circle" color="white" size={72} onPress={scheduleNotificationHandler} />
+						<IconButton icon="send" color="white" size={72} onPress={sendPushNotificationHandler} />
+					</>
+				);
 			},
 		});
-	}, [navigation, scheduleNotificationHandler]);
+	}, [navigation, scheduleNotificationHandler, sendPushNotificationHandler]);
 
 	useEffect(() => {
 		const subscription1 = Notifications.addNotificationReceivedListener((notification) => {
@@ -56,6 +77,35 @@ const CategoriesScreen = ({ navigation }) => {
 			subscription1.remove();
 			subscription2.remove();
 		};
+	}, []);
+
+	useEffect(() => {
+		async function configurePushNotifications() {
+			const { status } = await Notifications.getPermissionsAsync();
+			let finalStatus = status;
+
+			if (finalStatus !== "granted") {
+				const { status } = Notifications.requestPermissionsAsync();
+				finalStatus = status;
+			}
+
+			if (finalStatus !== "granted") {
+				Alert.alert("Permission required", "Push notifications need the appropriate permissions.");
+				return;
+			}
+
+			const pushTokenData = await Notifications.getExpoPushTokenAsync();
+			console.log(pushTokenData);
+
+			if (Platform.OS === "android") {
+				Notifications.setNotificationChannelAsync("default", {
+					name: "default",
+					importance: Notifications.AndroidImportance.DEFAULT,
+				});
+			}
+		}
+
+		configurePushNotifications();
 	}, []);
 
 	function renderCategoryItem(itemData) {
